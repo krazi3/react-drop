50bf1613cf9d2a14b4e1805c098790c86cc43309
krazi3 <awwabhaq@gmail.com>
2020-01-20 18:02:02 +0500

    Added a dropzone component with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Dropzone from './dropzone';
 
 function App() {
   return (
     <div className="container">
       <h1>Welcome</h1>
+      <Dropzone />
     </div>
   );
 }
---
+++ src/dropzone/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function () {
+  return (
+    <div className="wrapper">
+      <div className="dotted">
+        <h2>Drop your files here...</h2>
+      </div>
+    </div>
+  );
+}
